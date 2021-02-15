231bdac2321b8af5794d24c08b55076a62f48d2c
Ariyan0047 <aariyan0047@gmail.com>
2021-02-15 14:56:55 +0600

    Third Commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import "./App.css";
 // COMPONENT FUNCTION
 const App = () => {
   const title = "personal blog";
+  const score = 20;
+  const link = "http://www.google.com";
 
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
+        <p>{score === 50 ? "PASSED" : "FAILED"}</p>
+        <a href={link}>GOOGLE</a>
       </div>
     </div>
   );
 };
+
+// BOOLEAN AND OBJECTS CAN NOT BE OUTPUT THROUGH JSX DYNAMICALLY
 
 // EXPORTING COMPONENT FUNCTION
 export default App;
