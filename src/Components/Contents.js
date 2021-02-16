64dd205ceae146cb3d68b9d53190dd1274168dad
Ariyan0047 <aariyan0047@gmail.com>
2021-02-16 20:11:41 +0600

    Seventh Commit

--- src/Components/Contents.js
+++ src/Components/Contents.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Contents = () => {
   const info = [
@@ -6,22 +7,22 @@ const Contents = () => {
     { id: 2, name: "sendo" },
     { id: 3, name: "joe" },
     { id: 4, name: "kavin" },
+    { id: 5, name: "sendo" },
   ];
 
   // REACT HOOK
   const [blogs, setBlogs] = useState(info);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="contents">
       <h1>Blogs</h1>
-      <div className="lists">
-        {blogs.map((blog) => (
-          <div className="blog_preview" key={blog.id}>
-            <div className="blog_id">author id : {blog.id}</div>
-            <div className="blog_name">author name : {blog.name}</div>
-          </div>
-        ))}
-      </div>
+      <BlogList blogs={blogs} title="all blogs" handleDelete={handleDelete} />
+      {/*<BlogList blogs={blogs.filter((blog) => blog.name === "sendo")} title="sendo's blogs"/>*/}
     </div>
   );
 };
