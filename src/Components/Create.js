e587908bb93e7fe45a332f9d5418b88a2353a7b9
Ariyan0047 <aariyan0047@gmail.com>
2021-02-18 23:56:58 +0600

    Eleventh Commit

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,33 +1,56 @@
-import React from "react";
+import { useState } from "react";
 
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [blog, setBlog] = useState("");
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const createBlog = { title, blog, name };
+    console.log(createBlog);
+  };
+
   return (
     <div className="contents">
       <h1>create new blog</h1>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="title">
           <label htmlFor="title">blog title:</label>
           <input
             type="text"
             name="title"
             id="title"
-            placeholder="ENTER BLOG TITLE"
+            value={title}
             required
+            placeholder="ENTER BLOG TITLE"
+            onChange={(event) => setTitle(event.target.value)}
           />
         </div>
         <div className="blog">
           <label htmlFor="title">blog:</label>
-          <input
-            type="text"
+          <textarea
             name="blog"
             id="blog"
-            placeholder="ENTER BLOG"
             required
+            value={blog}
+            placeholder="ENTER AUTHOR NAME"
+            onChange={(event) => setBlog(event.target.value)}
+          ></textarea>
+        </div>
+        <div className="name">
+          <label htmlFor="name">author name:</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            required
+            value={name}
+            placeholder="ENTER BLOG"
+            onChange={(event) => setName(event.target.value)}
           />
         </div>
-        <div className="submit">
-          <input type="submit" value="SUBMIT" />
-        </div>
+        <input type="submit" value="SUBMIT" />
       </form>
     </div>
   );
